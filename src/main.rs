e408d2f6ca4cb0975a34862f29fe647a955e7737
Rafael <rafaelsora0@gmail.com>
2026-09-22 04:01:11 -0300

    upgrade rodio 22.2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use rand::seq::SliceRandom;
-use rodio::{Decoder, OutputStream, Sink};
+use rodio::{Decoder, MixerDeviceSink, Player};
 use std::{
     env,
     fs::{self, File},
     io::{self, BufReader, Write},
+    ops::ControlFlow::Break,
     path::{Path, PathBuf},
+    process::Command,
     sync::{mpsc, Arc, Mutex},
     thread::{self},
     time::Duration,
@@ -12,12 +14,22 @@ use std::{
 use trash;
 
 fn main() -> io::Result<()> {
-    let home_dir = env::var("HOME")
-        .or_else(|_| env::var("USERPROFILE"))
-        .expect("Could not get home directory");
-    let mut music_dir = PathBuf::from(home_dir);
-    music_dir.push("Músicas");
-    let path = Path::new(&music_dir);
+    let path: String;
+    println!("Aperte [Enter] para continuar OU digite qualquer tecla para selecionar o diretorio:");
+    let user_input = get_user_input();
+    if user_input.is_empty() {
+        path = fs::read_to_string("arquivo.txt").unwrap();
+    } else {
+        println!("digite o diretorio");
+
+        let input = get_user_input();
+
+        match fs::write("arquivo.txt", &input) {
+            Ok(_) => println!("Arquivo criado com sucesso!"),
+            Err(e) => println!("Erro ao criar o arquivo: {}", e),
+        }
+        path = input;
+    }
 
     let mut files = fs::read_dir(path)?
         .map(|res| res.map(|e| e.path()))
@@ -25,88 +37,76 @@ fn main() -> io::Result<()> {
 
     files = shuffle_files(files);
 
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Arc::new(Mutex::new(Sink::try_new(&stream_handle).unwrap()));
-    let files = Arc::new(Mutex::new(files));
-    let index = Arc::new(Mutex::new(0));
+    let sink_handle =
+        rodio::DeviceSinkBuilder::open_default_sink().expect("open default audio stream");
 
     let (tx, rx) = mpsc::channel::<String>();
 
-    let sink_clone = Arc::clone(&sink);
-    let files_clone = Arc::clone(&files);
-    let index_clone = Arc::clone(&index);
-
     let playback_thread = thread::spawn(move || {
-        playback_loop(rx, index_clone, files_clone, sink_clone);
+        playback_loop(rx, files, sink_handle);
     });
 
-    let input_thread = thread::spawn(move || {
-        input_lopp(tx);
-    });
+    input_lopp(tx);
 
     playback_thread.join().unwrap();
-    input_thread.join().unwrap();
-
     Ok(())
 }
 
 fn playback_loop(
     rx: mpsc::Receiver<String>,
-    index: Arc<Mutex<usize>>,
-    files: Arc<Mutex<Vec<PathBuf>>>,
-    sink: Arc<Mutex<Sink>>,
+    mut files: Vec<PathBuf>,
+    sink_handle: MixerDeviceSink,
 ) {
-    loop {
-        let mut idx = index.lock().unwrap();
-        let mut file_list = files.lock().unwrap();
+    let mut idx = 0;
 
-        if *idx >= file_list.len() {
+    loop {
+        if idx >= files.len() {
             println!("Não há mais músicas na lista.");
             break;
         }
 
-        play_music(&file_list[*idx], &mut sink.lock().unwrap()).unwrap();
+        let (player, _) = play_music(&files[idx], &sink_handle);
 
         loop {
-            if sink.lock().unwrap().empty() {
-                *idx += 1;
+            if player.empty() {
+                idx += 1;
                 break;
             }
 
             if let Ok(command) = rx.try_recv() {
                 match command.as_str() {
                     "next" => {
-                        if *idx + 1 < file_list.len() {
-                            *idx += 1;
+                        if idx + 1 < files.len() {
+                            idx += 1;
                         } else {
                             println!("Já estamos na última música.");
                         }
                         break;
                     }
                     "prev" => {
-                        if *idx > 0 {
-                            *idx -= 1;
+                        if idx > 0 {
+                            idx -= 1;
                         } else {
                             println!("Já estamos na primeira música.");
                         }
                         break;
                     }
                     "play" => {
-                        sink.lock().unwrap().play();
+                        player.play();
                         println!("musica tocando");
                     }
                     "pause" => {
-                        sink.lock().unwrap().pause();
+                        player.pause();
                         println!("musica pausada")
                     }
                     "delete" => {
-                        let file_to_delete = &file_list[*idx];
+                        let file_to_delete = &files[idx];
                         let _ = trash::delete(file_to_delete);
                         println!("Enviado para a lixeira: {:?}", file_to_delete);
 
-                        file_list.remove(*idx);
-                        if *idx >= file_list.len() && *idx > 0 {
-                            *idx -= 1;
+                        files.remove(idx);
+                        if idx >= files.len() && idx > 0 {
+                            idx -= 1;
                         }
                         break;
                     }
@@ -126,12 +126,12 @@ fn input_lopp(tx: mpsc::Sender<String>) {
     loop {
         let user_input = get_user_input();
         let command = match user_input.as_str() {
-            "D" => "next",
-            "A" => "prev",
-            "W" => "play",
-            "S" => "pause",
-            "L" => "delete",
-            "Q" => "quit",
+            "d" => "next",
+            "a" => "prev",
+            "w" => "play",
+            "s" => "pause",
+            "D" => "delete",
+            "q" => "quit",
             _ => continue,
         };
         tx.send(command.to_string()).unwrap();
@@ -141,22 +141,28 @@ fn input_lopp(tx: mpsc::Sender<String>) {
     }
 }
 
-fn play_music(file_path: &PathBuf, sink: &mut Sink) -> io::Result<()> {
-    sink.stop();
-    let file = BufReader::new(File::open(file_path)?);
-    sink.append(Decoder::new(file).unwrap());
+fn play_music(file_path: &PathBuf, sink_handle: &MixerDeviceSink) -> (Player, io::Result<()>) {
+    println!("Digite 'd' próximo, 'a' anterior, 'w' tocar, 's' pausar, 'D' deletar ou 'q' sair: ");
+    let file = match File::open(file_path) {
+        Ok(f) => BufReader::new(f),
+        Err(e) => {
+            eprintln!("Erro ao abrir a música: {}", e);
+            let empty_player = Player::connect_new(sink_handle.mixer());
+            return (empty_player, Err(e));
+        }
+    };
+    let player = rodio::play(&sink_handle.mixer(), file).unwrap();
     println!("Tocando arquivo: {:?}", file_path.file_name());
-    Ok(())
+    (player, Ok(()))
 }
 
 fn get_user_input() -> String {
-    println!("Digite 'd' próximo, 'a' anterior, 'w' tocar, 's' pausar, 'L' deletar ou 'q' sair: ");
     io::stdout().flush().expect("Falha ao limpar o buffer");
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("Falha ao ler a linha");
-    input.trim().to_uppercase().to_string()
+    input.trim().to_string()
 }
 
 fn shuffle_files(mut files: Vec<PathBuf>) -> Vec<PathBuf> {
